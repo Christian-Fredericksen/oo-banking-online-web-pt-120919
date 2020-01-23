b599f57834e378aa12a9775e8d920897b81c53df
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-23 20:47:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,8 @@ class Transfer
   
     sender.balance = sender.balance - @amount
     receiver.balance = receiver.balance + @amount
-  else
-    @status = "complete"
-  end 
-end
+    else
+      @status = "complete"
+    end 
+  end
 end
