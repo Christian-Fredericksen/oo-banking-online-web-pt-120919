3d56e9487d639aa20a6610859690a7489bdc91f1
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-23 21:52:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,11 +30,11 @@ class Transfer
    
    
    def reverse_transfer
-    # if @status == "complete"
+     if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
-    #end
+    end
    end 
 
   
