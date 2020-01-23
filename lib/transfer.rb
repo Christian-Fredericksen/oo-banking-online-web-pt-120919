ef3b61f0331803185b1ae1b7efe39abd2f57253a
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-23 21:44:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,12 @@ class Transfer
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
-  end
+   end
+   
+   
+   def reverse_transfer
+     
+   end 
 
   
 
