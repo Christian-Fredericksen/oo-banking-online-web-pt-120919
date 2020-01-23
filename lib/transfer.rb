af0360ac87e07e8d2c59669686a93b836720a416
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-23 21:04:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,22 +27,22 @@ class Transfer
   # end
   
    def execute_transaction
-    if @sender.valid? && @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
-      #@status = "rejected"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
 
-  def reverse_transfer
-    if @status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
-    end
-  end
+  # def reverse_transfer
+  #   if @status == "complete"
+  #     @sender.balance += @amount
+  #     @receiver.balance -= @amount
+  #     @status = "reversed"
+  #   end
+  # end
 
 end
