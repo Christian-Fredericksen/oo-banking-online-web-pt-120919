776dd395b668272ac36eba095684ba113773d4a9
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-23 20:46:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,9 +17,11 @@ class Transfer
   end
   
   def execute_transaction 
-    if @ 
+    if @staus == "pending"
+  
     sender.balance = sender.balance - @amount
     receiver.balance = receiver.balance + @amount
+  else
     @status = "complete"
   end 
 end
